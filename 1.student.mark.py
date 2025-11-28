4e006670ddfede71d12403b50df70838cc7184ba
Ducanh <phamducanh08022006@gmail.com>
2025-11-28 10:17:08 +0700

    Practice work 1

---
+++ 1.student.mark.py
@@ -0,0 +1,32 @@
+student_list = []
+course_list = []
+
+def add_student():
+    student = []
+    
+    ID = input("Input ID: ")
+    Name = input("Input name: ")
+    DoB = input("Input date of birth: ")
+    
+    student.append(ID)
+    student.append(Name)
+    student.append(DoB)
+    
+    student_list.append(student)
+
+def add_course():
+    course = []
+    
+    ID = input("Input ID: ")
+    Name = input("Input name: ")
+    
+    course.append(ID)
+    course.append(Name)
+    
+    course_list.append(course)
+
+me = {
+    "name",
+    "birthday"
+}
+print(me)
