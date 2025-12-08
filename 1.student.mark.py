a06cca181222693b04744831aad3099f8ebb9b7c
Alchery <phamducanh08022006@gmail.com>
2025-12-09 01:02:23 +0700

    Will finish later

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,31 +1,36 @@
 student_list = []
 course_list = []
 
-def add_student():
-    student = []
+def add_student(name: str, id: str, DoB: str):
+    s = {"Name": name,
+         "ID": id,
+         "Date of Birth": DoB}
     
-    ID = input("Input ID: ")
-    Name = input("Input name: ")
-    DoB = input("Input date of birth: ")
-    
-    student.append(ID)
-    student.append(Name)
-    student.append(DoB)
-    
-    student_list.append(student)
+    student_list.append(s)
 
-def add_course():
-    course = []
+def add_course(name: str, id: str):
+    c = {"Name": name,
+         "ID": id}
     
-    ID = input("Input ID: ")
-    Name = input("Input name: ")
+    course_list.append(c)
     
-    course.append(ID)
-    course.append(Name)
-    
-    course_list.append(course)
+def display_student():
+    for i in student_list:
+        for l in i:
+            print(f"Student {l}: {i[l]}")
+            
+def display_course():
+    for i in course_list:
+        for l in i:
+            print(f"Course {l}: {i[l]}")
 
-me = {
-    "name" : "Duc Anh",
-    "BoD" : "08/02/2006"
-}
+def add_score(sid: str, cid: str, mark):
+    #if sid not in 
+    pass
+
+def main():
+    add_student("Phạm Đức Anh","2410088","08/02/2006")
+    display_student()
+    
+if __name__ == "__main__":
+    main()
